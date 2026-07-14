8e8a597150980b3bd949dff2cb4e6bd7cb9e5169
Umar Abdullah <abdullahgilmannn@gmail.com>
2026-07-14 23:42:17 +0530

    Finished

--- Frontend/src/features/dashboard/UpdateProfile.jsx
+++ Frontend/src/features/dashboard/UpdateProfile.jsx
@@ -24,7 +24,7 @@ const UpdateProfile = () => {
           <div className="w-20 h-20 overflow-hidden bg-white border-2 rounded-full border-brand-purple/20">
             <img
               src={
-                user.avatar || "https://api.dicebear.com/7.x/bottts/svg?seed=Learnly",
+                user.avatar || "https://api.dicebear.com/7.x/bottts/svg?seed=Learnly"
               }
               alt={user.name}
               className="object-cover w-full h-full"
